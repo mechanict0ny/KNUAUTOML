5967125f0efd947fc57a140ebb7d35f0a56b79ef
Kwon-jinsol <ginsoly.knu@gmail.com>
2020-12-07 01:57:13 +0900

    수정 (test)
    thread 및 기타 수정

--- python_program/Sign.py
+++ python_program/Sign.py
@@ -19,8 +19,8 @@ import qimage2ndarray
 
 import tensorflow as tf
 import numpy as np
-
-
+import threading
+from time import sleep
 
 
 class Ui_MainWindow(object):
@@ -33,6 +33,12 @@ class Ui_MainWindow(object):
         self.pushButton = QPushButton(self.centralwidget)
         self.pushButton.setObjectName(u"pushButton")
         self.pushButton.setGeometry(QRect(310, 90, 111, 28))
+        self.pushButton.clicked.connect(self.bFunc)
+
+        self.pushButton2 = QPushButton(self.centralwidget)
+        self.pushButton2.setText('Clear')
+        self.pushButton2.setGeometry(QRect(430, 90, 50, 28))
+        self.pushButton2.clicked.connect(self.clearF)
 
         self.video_size = QSize(270,480)
         self.image_label = QLabel('Video Panel',self.centralwidget)
@@ -59,7 +65,8 @@ class Ui_MainWindow(object):
         self.checkBox.stateChanged.connect(self.checkBoxChange)
         #self.checkBox.toggle()
         self.label = QLabel(self.centralwidget)
-        self.label.setObjectName(u"label")
+        #self.label.setObjectName(u"label")
+        self.label.setText("-Label-")
         self.label.setGeometry(QRect(300, 170, 190, 50))
         font = QFont()
         font.setFamily(u"D2Coding")
@@ -67,11 +74,20 @@ class Ui_MainWindow(object):
         font.setBold(True)
         font.setWeight(150)
         self.label.setFont(font)
-        self.tableWidget = QTableWidget(self.centralwidget)
-        self.tableWidget.setObjectName(u"tableWidget")
-        self.tableWidget.setGeometry(QRect(300, 270, 190, 230))
-        # self.tableWidget.setRowCount(6)
-        # self.tableWidget.setColumnCount(2)
+
+        self.label2 = QLabel(self.centralwidget)
+        self.label2.setGeometry(QRect(310,280,160,220))
+        font = QFont()
+        font.setFamily(u"D2Coding")
+        font.setPointSize(12)
+        self.label2.setFont(font)
+        self.label2.setText("hello:\nhot:\nthanks\ncoffee:\n포장:\nnone:")
+
+
+        # self.tableWidget = QTableWidget(self.centralwidget)
+        # self.tableWidget.setObjectName(u"tableWidget")
+        # self.tableWidget.setGeometry(QRect(300, 270, 190, 230))
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QStatusBar(MainWindow)
         self.statusbar.setObjectName(u"statusbar")
@@ -89,7 +105,7 @@ class Ui_MainWindow(object):
 #endif // QT_CONFIG(tooltip)
         self.pushButton.setText(QCoreApplication.translate("MainWindow", u"\uc601\uc0c1 \ubd88\ub7ec\uc624\uae30", None))
         self.checkBox.setText(QCoreApplication.translate("MainWindow", u"\uce74\uba54\ub77c \uc774\uc6a9", None))
-        self.label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
+        #self.label.setText(QCoreApplication.translate("MainWindow", u"TextLabel", None))
     # retranslateUi
 
     # def sum_of_label(self, temp):
@@ -107,38 +123,163 @@ class Ui_MainWindow(object):
     #             max = value
     #     self.label.setText('%s:%f'%(max_label,max)) 
 
+    def clearF(self):
+
+        self.capture.release()
+        self.image_label.setText('Video Panel')
+        self.checkBox.setChecked(False)
+        self.label2.setText("hello:\nhot:\nthanks\ncoffee:\n포장:\nnone:")
+        self.label.setText("-Label-")
 
     def checkBoxChange(self,state):
 
         if state == Qt.Checked:
-            self.setup_camera()
+            #self.capture.release()
+            tc = threading.Thread(target=self.setup_camera1)
+            tc.daemon = True
+            tc.start()
+            #self.setup_camera()
         elif state != Qt.Checked:
-            self.timer.stop()
+            #self.timer.stop()
             self.capture.release()
             self.image_label.setText('Video Panel')
+            self.label2.setText("hello:\nhot:\nthanks\ncoffee:\n포장:\nnone:")
+            self.label.setText("-Label-")
         
-
-    def setup_camera(self):
+    def setup_camera1(self):
         """Initialize camera.
         """
-
+       
+        #self.capture = cv2.VideoCapture('C:/Users/degdo/Desktop/practice/KNU_AutoML/KNUAUTOML/python_program/test.mov')
         self.capture = cv2.VideoCapture(0)
         #self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, self.video_size.width())
         #self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.video_size.height())
+        
+        while True:
+            a, frame = self.capture.read()
+            if a:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                frame = cv2.flip(frame, 1)
+                copy_frame = frame.copy()
+                frame = copy_frame[:,320-135:320+135] 
 
-        self.timer = QTimer()
-        self.timer.timeout.connect(self.display_video_stream)
-        self.timer.start(30)
+                t2 = threading.Thread(target=self.labelf,args=(frame,))
+                t2.daemon = True
+                t2.start()
 
-    def display_video_stream(self):
-        """Read frame from camera and repaint QLabel widget.
+                image = qimage2ndarray.array2qimage(frame)  #SOLUTION FOR MEMORY LEAK
+                self.image_label.setPixmap(QPixmap.fromImage(image))
+                self.image_label.update()
+                
+                #sleep(0.001)
+
+            else:
+                break
+        self.capture.release()
+        self.image_label.setText('Video Panel')
+
+
+
+    # def setup_camera(self):
+    #     """Initialize camera.
+    #     """
+
+    #     self.capture = cv2.VideoCapture(0)
+    #     #self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, self.video_size.width())
+    #     #self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.video_size.height())
+
+    #     self.timer = QTimer()
+    #     self.timer.timeout.connect(self.display_video_stream)
+    #     self.timer.start(30)
+
+
+    # def display_video_stream(self):
+    #     """Read frame from camera and repaint QLabel widget.
+    #     """
+    #     _, frame = self.capture.read()
+    #     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    #     frame = cv2.flip(frame, 1)
+    #     copy_frame = frame.copy()
+    #     frame = copy_frame[:,320-135:320+135] 
+    #     # #
+    #     # img = cv2.resize(frame,dsize=(224,224))
+    #     # img = np.uint8(img)
+    #     # img = img[np.newaxis]
+    #     # interpreter.set_tensor(input_details[0]['index'], img)
+    #     # interpreter.invoke()
+    #     # output_data = interpreter.get_tensor(output_details[0]['index'])
+    #     # temp = output_data[0].tolist()
+    #     # #print(tolist1[9])
+    #     # #self.label.setText('%d'%tolist1[9])
+    #     # #sum_of_label(self, tolist1)
+    #     # # 레이블 출력
+    #     # dic = {}
+    #     # dic['hello'] = (temp[0] + temp[6] + temp[7] + temp[14])/4
+    #     # dic['hot'] = (temp[3] + temp[11])/2
+    #     # dic['thanks'] = (temp[4] + temp[8])/2
+    #     # dic['americano'] = (temp[2] + temp[10])/2
+    #     # dic['packing'] = (temp[5] + temp[9] + temp[12] + temp[13])/4
+    #     # dic['none'] = temp[1]
+    #     # max = 0
+    #     # for label, value in dic.items():
+    #     #     if value > max:
+    #     #         max_label = label
+    #     #         max = value
+    #     # self.label.setText('%s'%max_label) #:\n%f  ,max)
+    #     # # di_key = dic.keys()
+    #     # # di_val = dic.values()
+    #     # # self.tableWidget.setItem(0,0,QTableWidgetItem('%s'%di_key[0]))
+    #     # # self.tableWidget.setItem(0,1,QTableWidgetItem("%s"%di_val[0]))
+    #     # ##
+    #     # #
+        
+    
+    #     image = qimage2ndarray.array2qimage(frame)  #SOLUTION FOR MEMORY LEAK
+    #     self.image_label.setPixmap(QPixmap.fromImage(image))
+    
+
+    def bFunc(self):
+        #self.capture.release()
+        filename = QFileDialog.getOpenFileName(MainWindow,"open",'./')
+        t1 = threading.Thread(target=self.setup_camera2,args=(filename[0],))
+        t1.daemon = True
+        t1.start()
+
+    def setup_camera2(self, fname):
+        """Initialize camera.
         """
-        _, frame = self.capture.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = cv2.flip(frame, 1)
-        copy_frame = frame.copy()
-        frame = copy_frame[:,320-135:320+135] 
-        #
+       
+        #self.capture = cv2.VideoCapture('C:/Users/degdo/Desktop/practice/KNU_AutoML/KNUAUTOML/python_program/test.mov')
+        self.capture = cv2.VideoCapture(fname)
+        #self.capture.set(cv2.CAP_PROP_FRAME_WIDTH, self.video_size.width())
+        #self.capture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.video_size.height())
+        
+        while True:
+            a, frame = self.capture.read()
+            if a:
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                #frame = cv2.flip(frame, 1)
+                frame = cv2.rotate(frame,cv2.ROTATE_90_CLOCKWISE)
+                frame = cv2.resize(frame, dsize=(270,480))    
+
+                t2 = threading.Thread(target=self.labelf,args=(frame,))
+                t2.daemon = True
+                t2.start()
+
+                image = qimage2ndarray.array2qimage(frame)  #SOLUTION FOR MEMORY LEAK
+                self.image_label.setPixmap(QPixmap.fromImage(image))
+                self.image_label.update()
+                
+                #sleep(0.001)
+
+            else:
+                break
+        self.capture.release()
+        self.image_label.setText('Video Panel')
+
+    def labelf(self, frame):
+        interpreter = tf.lite.Interpreter(model_path="C:/Users/degdo/workspace/py_gui/1204_model-export_icn_tflite-vision_model_edge2_high-2020-12-04T03_01_01.444480Z_model.tflite")
+        interpreter.allocate_tensors()
         img = cv2.resize(frame,dsize=(224,224))
         img = np.uint8(img)
         img = img[np.newaxis]
@@ -149,31 +290,23 @@ class Ui_MainWindow(object):
         #print(tolist1[9])
         #self.label.setText('%d'%tolist1[9])
         #sum_of_label(self, tolist1)
-##
+        # 레이블 출력
         dic = {}
-        dic['hello'] = (temp[0] + temp[6] + temp[7] + temp[14])/4
-        dic['hot'] = (temp[3] + temp[11])/2
-        dic['thanks'] = (temp[4] + temp[8])/2
-        dic['americano'] = (temp[2] + temp[10])/2
-        dic['packing'] = (temp[5] + temp[9] + temp[12] + temp[13])/4
+        dic['hello'] = max(temp[0] , temp[6] , temp[7] , temp[14])
+        dic['hot'] = max(temp[3] , temp[11])
+        dic['thanks'] = max(temp[4] , temp[8])
+        dic['coffee'] = max(temp[2] , temp[10])
+        dic['포장'] = max(temp[5] , temp[9] , temp[12] , temp[13])
         dic['none'] = temp[1]
-        max = 0
+        maxv = 0
         for label, value in dic.items():
-            if value > max:
+            if value > maxv:
                 max_label = label
-                max = value
+                maxv = value
         self.label.setText('%s'%max_label) #:\n%f  ,max)
-        # di_key = dic.keys()
-        # di_val = dic.values()
-        # self.tableWidget.setItem(0,0,QTableWidgetItem('%s'%di_key[0]))
-        # self.tableWidget.setItem(0,1,QTableWidgetItem("%s"%di_val[0]))
-##
-        #
-        
+        self.label2.setText("hello:\t%d\nhot:\t%d\nthanks:\t%d\ncoffee:\t%d\n포장:\t%d\nnone:\t%d"%(dic['hello'],dic['hot'],dic['thanks'],dic['coffee'],dic['포장'],dic['none']))
     
-        image = qimage2ndarray.array2qimage(frame)  #SOLUTION FOR MEMORY LEAK
-        self.image_label.setPixmap(QPixmap.fromImage(image))
-
+    
 
 if __name__ == "__main__":
 
